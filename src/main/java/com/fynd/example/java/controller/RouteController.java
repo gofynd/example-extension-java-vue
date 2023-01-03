fc5ea66e9391bf70aaad5b2bc1df6b8e08615c59
Bhargav Prajapati <bhargavprajapati@gofynd.com>
2023-01-03 15:12:47 +0530

    changes

--- src/main/java/com/fynd/example/java/controller/RouteController.java
+++ src/main/java/com/fynd/example/java/controller/RouteController.java
@@ -1,4 +1,4 @@
-package com.fynd.groot.java.controller;
+package com.fynd.example.java.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
