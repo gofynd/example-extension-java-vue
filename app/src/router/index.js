d705cc792adbcfd0ae8603bac23065bd2f5c7320
jinalviranii <jinalvirani@gofynd.com>
2024-06-19 18:04:55 +0530

    Change UI for boilerplate

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -11,15 +11,12 @@ const routes = [
     beforeEnter: routeGuard,
     component: Home
   },
-  // {
-  //   path: '/company/:company_id/about',
-  //   beforeEnter: routeGuard,
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/company/:company_id/application/:application_id',
+    name: 'Home',
+    beforeEnter: routeGuard,
+    component: Home
+  }
 ]
 
 const router = new VueRouter({
