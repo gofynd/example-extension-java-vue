4baa500fe4c6d913c33b26858570af11f5b896ad
girishbharambe <girishbharambe@gofynd.com>
2024-08-08 16:20:18 +0530

    tomcat server reload

--- src/main/java/com/fynd/example/java/ExampleJavaApplication.java
+++ src/main/java/com/fynd/example/java/ExampleJavaApplication.java
@@ -55,7 +55,6 @@ public class ExampleJavaApplication {
 	});
 
 	public static void main(String[] args) {
-		System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(ExampleJavaApplication.class, args);
 	}
 
